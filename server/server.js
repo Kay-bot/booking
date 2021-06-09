9d1ab02aaa0a7918ffcf6ccbfabdb1940c5474ea
KAY S <KS.KWANRUAN@GMAIL.com>
2021-06-09 22:08:43 +1000

    test

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import {readdirSync} from 'fs'
+import cors from 'cors'
+import mongoose from 'mongoose'
 const morgan = require('morgan') 
 require('dotenv').config()
 
 const app = express()
 
 // middlewares
+app.use(cors())
 app.use(morgan('dev'))
 
 // route middleware
