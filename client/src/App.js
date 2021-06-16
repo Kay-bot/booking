f8ff19c174e26c9a3c57a3ccf2eb12e8d96d212a
KAY S <KS.KWANRUAN@GMAIL.com>
2021-06-16 17:02:17 +1000

    react-toastify for notification

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,33 @@
-import {BrowserRouter, Switch, Route} from "react-router-dom"
-import Home from './booking/Home'
-import Login from './auth/Login'
-import Regisrter from './auth/Register'
-import TopNav from './components/TopNav'
-
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './booking/Home';
+import Login from './auth/Login';
+import Regisrter from './auth/Register';
+import TopNav from './components/TopNav';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  return (<BrowserRouter>
- <TopNav/>
-    <Switch>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/login" component={Login}/>
-      <Route exact path="/register" component={Regisrter}/>
-    </Switch>
-  </BrowserRouter>)
+  return (
+    <BrowserRouter>
+      <TopNav />
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Regisrter} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- client/src/auth/Register.js
+++ client/src/auth/Register.js
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import RegisterForm from '../components/RegisterForm';
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
-const Register = () => {
+const Register = ({ history }) => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -16,8 +17,11 @@ const Register = () => {
         password,
       });
       console.log('Register User:', res);
+      toast.success('Register success. Please login.');
+      history.push('/login');
     } catch (err) {
       console.log(err);
+      if (err.response.status === 400) toast.error(err.response.data);
     }
   };
 
@@ -26,6 +30,7 @@ const Register = () => {
       <div className="container-fluid bg-secondary p-5 text-center">
         <h1>Register</h1>
       </div>
+
       <div>
         <div className="container">
           <div className="row">
