4d05b31f560d3317423df38dbae1c14d4f0e98a1
KAY S <KS.KWANRUAN@GMAIL.com>
2021-06-18 16:24:37 +1000

    user authentication

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,5 @@
 import User from '../models/user';
+import jwt from 'jsonwebtoken';
 
 export const register = async (req, res) => {
   console.log(req.body);
@@ -21,5 +22,38 @@ export const register = async (req, res) => {
   } catch (err) {
     console.log('FAILED TO CREATE USER', err);
     return res.status(400).send('Error, Try again');
+  }
+};
+
+export const login = async (req, res) => {
+  // console.log(req.body);
+  const { email, password } = req.body;
+  try {
+    // if user with that email exist
+    let user = await User.findOne({ email }).exec();
+    // console.log('USER EXIST', user);
+    if (!user) res.status(400).send(`User with that email is not found`);
+    // compare password
+    user.comparePassword(password, (err, match) => {
+      console.log(`Compare password in login error`, err);
+      if (!match || err) return res.status(400).send('Wrong password');
+      // Generate token
+      let token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: 24 * 60 * 60,
+      });
+      res.json({
+        token,
+        user: {
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          createdAt: user.createdAt,
+          updatedAt: user.updatedAt,
+        },
+      });
+    });
+  } catch (err) {
+    console.log(`Login error`, err);
+    res.status(400).send(`Signin failed`);
   }
 };
